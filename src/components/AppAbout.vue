aeaf2afd562270c0d06c8c0c4a3a1fd295ff0309
rosselton <rebecca.osselton@newcastle.ac.uk>
2020-09-04 11:58:07 +0100

    adding CC licence

--- src/components/AppAbout.vue
+++ src/components/AppAbout.vue
@@ -17,7 +17,8 @@
 
       <p>For further information or to get in touch about DAAWN, please contact <span class="special">fiona.menger@ncl.ac.uk</span>. </p>
 
-
+      <p><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</p>
+     <p>    <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/80x15.png" /></a></p>
 
 
       <div class="level" >
