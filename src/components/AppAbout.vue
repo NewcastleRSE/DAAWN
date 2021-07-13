50a4e678af809b08c8c21eba77e3af2d51c9d8ac
rosselton <rebecca.osselton@ncl.ac.uk>
2021-07-13 11:48:29 +0100

    update to CC image

--- src/components/AppAbout.vue
+++ src/components/AppAbout.vue
@@ -26,7 +26,7 @@
 
       <p class="text"><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Digitised Assessment for Aphasia of WritiNg (DAAWN)</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://daawn.ncldata.dev/" property="cc:attributionName" rel="cc:attributionURL">https://daawn.ncldata.dev/</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.</p>
 
-      <p class="text"><a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a></p>
+      <p class="text"><a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons Licence" style="border-width:0; height: 35px;" src="https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-nc.png" /></a></p>
 
 
       <div class="level" >
